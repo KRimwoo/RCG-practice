a80d847803ceaf9a3c9c32f318e5ff0b9b7d198d
Woorim Kim <wrkimlimi@gmail.com>
2023-06-16 15:22:37 +0900

    POST request, useRef

--- 01-starting-project 3/src/App.js
+++ 01-starting-project 3/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -12,21 +13,35 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("http://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://rcg-section14-http-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      console.log(data);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -37,6 +52,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://rcg-section14-http-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie), //change JS objects, array into JSON
+        headers: {
+          "Content-Type": "application/json", //a lot of REST API might require headers
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
   //if (movies.length > 0) {
   //  content = <MoviesList movies={movies} />;
   //}
@@ -49,7 +78,7 @@ function App() {
   //  content = <p>Loading...</p>;
   //}
 
-  //if (movies.lenth === 0) {
+  //if (movies.length === 0) {
   //  content = <p>Found no movies.</p>;
   //}
 
@@ -64,6 +93,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>

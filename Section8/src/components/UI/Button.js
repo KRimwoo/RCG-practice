fd6444d92924eaaab71b1a1f7f5f830a5e1c3ae6
Woorim Kim <woorikim@student.42seoul.kr>
2023-07-22 17:06:07 +0900

    section8

---
+++ Section8/src/components/UI/Button.js
@@ -0,0 +1,15 @@
+import classes from './Button.module.css';
+
+const Button = props => {
+	return (
+    <Button 
+      className={classes.button} 
+      type={props.type || 'button'} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </Button>
+  )
+};
+
+export default Button;

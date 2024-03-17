77fb0bb25be9296ca7f1f56cdcff5060c9235394
Woorim Kim <wrkimlimi@gmail.com>
2024-03-17 13:19:23 +0900

    rename folder

--- my-app-hooks/src/App.js
+++
@@ -1,8 +0,0 @@
-import UseInputApp from "./components/customHook/UseInputApp";
-import BankApp from "./components/useReducer/BankApp";
-
-function App() {
-  return <UseInputApp/>
-}
-
-export default App;
